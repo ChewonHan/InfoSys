6911d7c4bd3154977ffdf03a2cde09dd0d1b5762
ChewonHan <about0322@naver.com>
2023-02-13 11:55:42 +0800

    finished pset1  - full score but need to consider the running time

--- JavaLib/src/main/java/problemset1a/Pset1.java
+++ JavaLib/src/main/java/problemset1a/Pset1.java
@@ -1,56 +1,56 @@
-package problemset1a;
-
-import java.util.*;
-
-public class Pset1 {
-
-//    /**
-//     * @param args
-//     */
-
-    public static void main(String[] args) {
-        System.out.println(isAllCharacterUnique("abcdefghijklmnopqrstuvABC"));
-        //System.out.println(Pset1.isAllCharacterUnique("abcdefgghijklmnopqrstuvABC"));
-        //System.out.println(Pset1.isPermutation("@ab", "a@b"));
-        //System.out.println(Pset1.isPermutation("abcd", "bcdA"));
-
-    }
-
-
-    public static boolean isAllCharacterUnique(String sIn) {
-        boolean ans = true;
-
-        // convert sIn into an array with characters
-        ArrayList<Character> charList = new ArrayList<>();
-        for (int i = 0; i < sIn.length(); i++) {
-            charList.add(sIn.charAt(i));
-        }
-        System.out.println(charList);
-        System.out.println(charList.size());
-
-        for (char j = 0; j < charList.size(); j ++ ){
-            // check whether have the char[j] is in charList[j, len(charList)]
-            ArrayList<Character> shortList = (ArrayList<Character>) charList.subList( j + 1 , charList.size());
-            ans = !(shortList.contains(charList.get(j)));
-
-            if (ans == false){
-                break;
-            }
-        }
-        return ans;
-    }
-
-        public static boolean isPermutation (String sIn1, String sIn2){
-            //todo: add your implementation
-
-            if (sIn1.length() == sIn2.length()){
-                for (int i = 0; i < sIn1.length(); i++){
-
-                }
-            }
-
-            return false;
-        }
-    }
-
-
+//package problemset1a;
+//
+//import java.util.*;
+//
+//public class Pset1 {
+//
+////    /**
+////     * @param args
+////     */
+//
+//    public static void main(String[] args) {
+//        System.out.println(isAllCharacterUnique("abcdefghijklmnopqrstuvABC"));
+//        //System.out.println(Pset1.isAllCharacterUnique("abcdefgghijklmnopqrstuvABC"));
+//        //System.out.println(Pset1.isPermutation("@ab", "a@b"));
+//        //System.out.println(Pset1.isPermutation("abcd", "bcdA"));
+//
+//    }
+//
+//
+//    public static boolean isAllCharacterUnique(String sIn) {
+//        boolean ans = true;
+//
+//        // convert sIn into an array with characters
+//        ArrayList<Character> charList = new ArrayList<>();
+//        for (int i = 0; i < sIn.length(); i++) {
+//            charList.add(sIn.charAt(i));
+//        }
+//        System.out.println(charList);
+//        System.out.println(charList.size());
+//
+//        for (char j = 0; j < charList.size(); j ++ ){
+//            // check whether have the char[j] is in charList[j, len(charList)]
+//            ArrayList<Character> shortList = (ArrayList<Character>) charList.subList( j + 1 , charList.size());
+//            ans = !(shortList.contains(charList.get(j)));
+//
+//            if (ans == false){
+//                break;
+//            }
+//        }
+//        return ans;
+//    }
+//
+//        public static boolean isPermutation (String sIn1, String sIn2){
+//            //todo: add your implementation
+//
+//            if (sIn1.length() == sIn2.length()){
+//                for (int i = 0; i < sIn1.length(); i++){
+//
+//                }
+//            }
+//
+//            return false;
+//        }
+//    }
+//
+//
--- JavaLib/src/main/java/problemset1a/Pset1_1.java
+++ JavaLib/src/main/java/problemset1a/Pset1_1.java
@@ -1,10 +1,30 @@
 package problemset1a;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+
 public class Pset1_1 {
+
+    public static void main(String[] args) {
+        System.out.println(isAllCharacterUnique("abcdefghijklmnopqrstuvABC"));
+        System.out.println(isAllCharacterUnique("abcdefgghijklmnopqrstuvABC"));
+        System.out.println(isPermutation("@ab", "a@b"));
+        System.out.println(isPermutation("abcd", "bcdA"));
+    }
     public static boolean isAllCharacterUnique(String sIn) {
         //todo: add your implementation
 
-        HashSet
+        HashSet<Character> set = new HashSet<>();
+        for (Character ch : sIn.toCharArray()){
+            set.add(ch);
+        }
+
+        if (set.size() == sIn.length()){
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
 
@@ -12,5 +32,18 @@ public class Pset1_1 {
 
     public static boolean isPermutation(String sIn1, String sIn2) {
         //todo: add your implementation
+        ArrayList<Character> arr = new ArrayList<>();
+
+        for (Character ch: sIn2.toCharArray()){
+            arr.add(ch);
+        }
+
+        for (Character ch : sIn1.toCharArray()){
+            if (!arr.contains(ch)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
+
