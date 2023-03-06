a78992f7ae283d563875cacd988e7e3835a982ec
ChewonHan <about0322@naver.com>
2023-03-06 21:56:40 +0800

    First Set up 2

--- app/src/main/java/com/example/myapplicationpractice/MainActivity.java
+++ app/src/main/java/com/example/myapplicationpractice/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplicationpractice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.javaprograms.R;
 
@@ -12,6 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // TODO 1 - Write a logcat statement
+        Log.d("hi","debugging");
 
     }
 }
